05ca67b9a55fab257704b82dd39237c5729ae12b
Michaelson44 <149709538+Michaelson44@users.noreply.github.com>
2024-10-31 12:45:22 -0700

    status update

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,5 @@
 const {verifyAdmin, verifyAndAuth, verify} = require("../middleware/verify");
-const {getSingleOrder, getOrder, postOrder, deleteOrder, updateOrder} = require("../controller/order");
+const {getSingleOrder, getOrder, postOrder, deleteOrder, updateOrder, updateStatus} = require("../controller/order");
 const router = require("express").Router();
 
 router.get("/orders", verify, getOrder);
@@ -7,6 +7,6 @@ router.get("/orders", verify, getSingleOrder);
 router.post("/orders", verify, postOrder);
 router.put("/orders/:id", verifyAndAuth, updateOrder);
 router.delete("/orders/:id", verifyAndAuth, deleteOrder);
-router.put("/order-status/:id", verifyAndAuth, deleteOrder);
+router.put("/order-status/:id", verifyAndAuth, updateStatus);
 
 module.exports = router;
